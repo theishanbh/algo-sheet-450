9fc4f2aaa34233dc1e5f2f7b66e8e68723cf20a0
theishanbh <theishanbh@gmail.com>
2022-05-06 00:17:00 +0530

    first code done

--- 1. Reverse array/program.cpp
+++ 1. Reverse array/program.cpp
@@ -1,6 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    cout << "" << endl;
+int main()
+{
+    int n = 5;
+    int arr[n] = {1, 2, 3, 4, 5};
+    int arrRev[n];
+    for (int i = 0; i < n; i++)
+    {
+        arrRev[n - i - 1] = arr[i];
+    }
+    for (int i = 0; i < n; i++)
+    {
+        cout << arrRev[i] << " ";
+    }
+
     return 0;
 }
